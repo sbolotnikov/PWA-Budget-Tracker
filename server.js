04fcf430c5080dd9e6f1cf63a12809879576ed6e
Sergey Bolotnikov <sbolotnikov@gmail.com>
2020-10-08 18:41:26 -0400

    heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,8 +15,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_ATLAS_URI ||"mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true, 
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
@@ -24,5 +26,5 @@ mongoose.connect("mongodb://localhost/budget", {
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running link http://localhost:${PORT}!`);
 });
