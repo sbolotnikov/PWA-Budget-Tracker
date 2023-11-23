9390c23d9fe81a425553ea3d90decdec13bb0e5d
Sergey Bolotnikov <sbolotnikov@gmail.com>
2023-11-23 10:53:16 -0500

    add proxy for Whats for dinner

--- routes/api.js
+++ routes/api.js
@@ -31,4 +31,59 @@ router.get("/api/transaction", (req, res) => {
     });
 });
 
+const axios = require("axios");
+
+// route to get response from Google Places API
+router.get("/proxy/api/0/v1:link", function (req, res) {
+  let url_1 = req.params.link.slice(7) + process.env.APIKey;
+  console.log(url_1)
+  axios.get(url_1)
+    .then(function (response) {   
+        // fixing CORS
+        console.log(response.data);
+        res.header('Access-Control-Allow-Origin', '*');
+        res.send(response.data);     
+    });
+
+});
+
+// route that gets the link to google picture by reference
+router.get("/proxy/api/1/v1:link", function (req, res) {
+  let url_1 = req.params.link.slice(7) + process.env.APIKey;
+  fetch(url_1,{
+    cache: 'no-cache',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    }
+  }).then(response=>{
+
+      res.header('Access-Control-Allow-Origin', '*');
+      res.send(response.url);
+    }).catch(err => {
+      console.log(err);
+    });
+});
+// route to get information from spoonacular API
+router.get("/proxy/api/key/:set/:link", function (req, res) {
+  let url_1 = '';
+  if (req.params.set === '0') {
+    // url for the search results from spoonacular API 
+    url_1 = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + process.env.APISpoon + req.params.link;
+  }
+  // url for recepies ingridients route
+  else if (req.params.set === '1') { url_1 = `https://api.spoonacular.com/recipes/${req.params.link}/ingredientWidget.json?apiKey=` + process.env.APISpoon; }
+  // url for recepies detailed route
+  else if (req.params.set === '2') { url_1 = `https://api.spoonacular.com/recipes/${req.params.link}/analyzedInstructions?apiKey=` + process.env.APISpoon; }
+  console.log(url_1)
+  axios
+    .get(url_1)
+    .then(function (response) {
+      // fixing CORS
+      res.header('Access-Control-Allow-Origin', '*');
+      res.send(response.data);
+    });
+});
+
+
 module.exports = router;
